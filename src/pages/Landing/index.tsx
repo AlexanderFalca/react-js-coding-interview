93e352ade525daf7bee1ceac7c6acccf2671f29b
Alexander Falca <x_mordred@hotmail.com>
2021-02-11 15:45:44 +0000

    Feat - button - Fetch Random New Data User

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,17 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import UserInfosItem, { UserInfos } from './UserInfosItem';
 import api from '../../services/api';
 
 const Landing: React.FC = () => {
   const [randomUserDataJSON, setRandomUserDataJSON] = useState<string>();
-  const [usersInfos, setUsersInfos] = useState([]);
-  // const [page, setPage] = useState(1);
-
+  const [usersInfos, setUsersInfos] = useState<UserInfos[]>([]);
+  const [page, setPage] = useState(1);
+  /* const prevPage = useCallback(() => {
+    if (page === 1) return;
+    const pageNumber = page - 1;
+    setPage(pageNumber);
+  }, [page]); */
+  const nextPage = useCallback(() => {
+    if (page === undefined) return;
+    const pageNumber = page + 1;
+    setPage(pageNumber);
+  }, [page]);
+  /* const deleteGridItem = useCallback(
+    async id => {
+      try {
+        await api.delete(`/products/${id}`);
+        const bla = products.filter(
+          (deliveryman: IProduct) => deliveryman.id !== id,
+        );
+        setFilteredProducts(bla);
+      } catch (err) {
+        console.log('Erro');
+      }
+    },
+    [products],
+  ); */
   useEffect(() => {
-    async function fetchData(pageNumber = 1) {
-      const request = await api.get(`/api/?page=${pageNumber}`);
+    async function fetchData(pageNumber = page) {
+      const request = await api.get(`/api?page=${pageNumber}`);
       setRandomUserDataJSON(JSON.stringify(request.data, null, 2) || 'No user data found.');
-      setUsersInfos(request.data.results);
+      if (request.data.results === undefined) return;
+
+      setUsersInfos([
+        ...usersInfos,
+        ...request.data.results,
+      ]);
     }
     try {
       fetchData();
@@ -19,7 +47,7 @@ const Landing: React.FC = () => {
       // eslint-disable-next-line no-console
       console.error(error);
     }
-  }, []);
+  }, [page]);
   /* const [user, setUser] = useState<IUser>();
 
   useEffect(() => {
@@ -33,7 +61,10 @@ const Landing: React.FC = () => {
   }, [id]); */
   return (
   <div>
-        <h1> Feth data test </h1>
+    <div>
+        <strong> Fetch Random Data User </strong>
+    </div>
+
         <div>
           {usersInfos.map((user: UserInfos) => (
               <UserInfosItem
@@ -42,7 +73,20 @@ const Landing: React.FC = () => {
                />
           ))}
         </div>
+
         <div>
+                {/* <button disabled={page === 1} onClick={prevPage}>
+                    Before
+                    </button> */}
+                <button disabled={page === undefined} onClick={nextPage}>
+                    Next User
+                    </button>
+            </div>
+
+        <div>
+          <div>
+            <h1> JSON - Last user's information </h1>
+          </div>
           {randomUserDataJSON}
         </div>
   </div>
